e54fe1a281d01aca90ff4c4bd9747afa74e76c40
AlBundy33 <>
2021-05-02 13:40:52 +0200

    removed 100% NOT working debounce :-)

--- TruckShifter.ino
+++ TruckShifter.ino
@@ -5,8 +5,6 @@
 #define SPLITTER      19
 #define RANGE         20
 
-#define DEBOUNCE 50
-
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
                    JOYSTICK_TYPE_JOYSTICK, 3, 0,
                    false, false, false, false, false, false,
@@ -20,34 +18,10 @@ void setup() {
     Joystick.begin();
 }
 
-unsigned long ms_cs = 0;
-unsigned long ms_splitter = 0;
-unsigned long ms_range = 0;
-
 void loop() {
     int cs = !digitalRead(COMFORT_SHIFT);
     int splitter = !digitalRead(SPLITTER);
     int range = !digitalRead(RANGE);
-
-    // debounce
-    if (cs) {
-        if (ms_cs == 0)
-            ms_cs = millis();
-        else if (ms_cs < DEBOUNCE)
-            cs = LOW;
-    }
-    if (splitter) {
-        if (ms_splitter == 0)
-            ms_splitter = millis();
-        else if (ms_splitter < DEBOUNCE)
-            splitter = LOW;
-    }
-    if (range) {
-        if (ms_range == 0)
-            ms_range = millis();
-        else if (ms_range < DEBOUNCE)
-            range = LOW;
-    }
 
     Joystick.setButton(0, cs);
     Joystick.setButton(1, splitter);
